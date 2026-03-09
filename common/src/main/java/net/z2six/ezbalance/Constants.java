95af8720c47081f75d68dabcef0992f106110784
Z2SIX <bathofredemption@duck.com>
2026-03-09 21:48:26 +0100

    init

---
+++ common/src/main/java/net/z2six/ezbalance/Constants.java
@@ -0,0 +1,11 @@
+package net.z2six.ezbalance;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "ezbalance";
+	public static final String MOD_NAME = "EZBalance";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/net/z2six/ezbalance/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package net.z2six.ezbalance.platform;
+
+import net.z2six.ezbalance.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
